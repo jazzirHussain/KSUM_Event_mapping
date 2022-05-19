7e1a5eed31f32756b4b0ca3393c2af56debd58de
Jazzir Hussain <jasirhussain2002@gmail.com>
2022-05-19 16:41:21 +0530

    'lates'

--- app/src/main/java/com/example/ksumeventmapping/FirstFragment.java
+++ app/src/main/java/com/example/ksumeventmapping/FirstFragment.java
@@ -4,13 +4,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 public class FirstFragment extends Fragment {
-
+    private static ImageView img;
+    private int c;
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
@@ -23,12 +25,12 @@ public class FirstFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
+        img = (ImageView) getView().findViewById(R.id.outsideImageView);
+        img.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
                 NavHostFragment.findNavController(FirstFragment.this)
                         .navigate(R.id.action_FirstFragment_to_SecondFragment);
-
             }
         });
     }
