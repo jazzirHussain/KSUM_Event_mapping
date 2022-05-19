e0082b767b8f54d8841683b0e6b5645a29ab70be
antony <antonymaliakal02@gmail.com>
2022-05-19 15:26:08 +0530

    dbSuccess

--- app/src/main/java/com/example/ksumeventmapping/MainActivity.java
+++ app/src/main/java/com/example/ksumeventmapping/MainActivity.java
@@ -22,8 +22,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //Toolbar toolbar = findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
         SQLiteDatabase db= new DataBaseHelper(MainActivity.this).getWritableDatabase();
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
